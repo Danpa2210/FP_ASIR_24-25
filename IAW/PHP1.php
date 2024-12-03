45c25db0511f66a9da6b3c402cfdf2946b75de6a
Miller <dherasi01@suarezdefigueroa.es>
2024-12-03 14:13:58 +0100

    PHP1

---
+++ IAW/PHP1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP1</title>
+    </head>
+    <body>
+¿Dónde se ejecuta el código PHP: en el servidor o en el cliente?
+    <?php echo "<p>Se ejecuta en el servidor</p>"; ?>
+
+¿Qué tipo de contenido puede generar PHP?
+Se pueden hacer cookies, formularios, emails y demás...
+
+¿Dónde se ejecuta el código PHP: en el servidor o en el cliente?
+¿Dónde se ejecuta el código PHP: en el servidor o en el cliente?
+
+    </body>
+</html>
