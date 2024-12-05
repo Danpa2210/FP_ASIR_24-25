af0fcea7c325076205760beea0bf54aed2a22166
Miller <dherasi01@suarezdefigueroa.es>
2024-12-05 14:03:45 +0100

    PHP3

---
+++ IAW/PHP3.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP1</title>
+    </head>
+    <body>
+    ¿Qué significa <?php?
+    //Esta linea de codigo sirve para escribir un mensaje por medio de echo
+    <?php echo "<p>Es la forma que tiene el lenguaje php de comenzar una linea de codigo.</p>"; ?>
+
+    //Y aqui para mas de lo mismo 
+    ¿Para qué sirve echo?
+    <?php echo "<p>Es lo que se usa en php para escribir un mensaje.</p>"; ?>
+
+    /*Ahora tambien se usa echo para escribir un mensaje explicativo*/
+    ¿Qué ocurre cuando ejecutas el archivo en un servidor?
+    <?php echo "<p>Se ejecuta y posteriormente nos muestra el propio html.</p>"; ?>
+
+    </body>
+</html
