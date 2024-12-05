861452640fca519242bc197adf7f58b2cd7f3603
Miller <dherasi01@suarezdefigueroa.es>
2024-12-05 13:56:44 +0100

    PHP2

---
+++ IAW/PHP2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP1</title>
+    </head>
+    <body>
+    ¿Qué significa <?php?
+    <?php echo "<p>Es la forma que tiene el lenguaje php de comenzar una linea de codigo.</p>"; ?>
+
+    ¿Para qué sirve echo?
+    <?php echo "<p>Es lo que se usa en php para escribir un mensaje.</p>"; ?>
+
+    ¿Qué ocurre cuando ejecutas el archivo en un servidor?
+    <?php echo "<p>Se ejecuta y posteriormente nos muestra el propio html.</p>"; ?>
+
+    </body>
+</html
