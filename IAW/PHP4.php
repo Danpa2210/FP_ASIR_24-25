a179c24b18db5abc9b7723bfce08995d89faac5c
Miller <dherasi01@suarezdefigueroa.es>
2024-12-05 14:15:53 +0100

    PHP4

---
+++ IAW/PHP4.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP1</title>
+    </head>
+    <body>
+    <?php 
+    
+    echo "<p>Para abrir una linea de php se tiene que empezar con <?php y terminar con ?> </p>"; ?>
+
+<?php echo "<p>PHP is running</p>"; ?>
+
+    </body>
+</html
